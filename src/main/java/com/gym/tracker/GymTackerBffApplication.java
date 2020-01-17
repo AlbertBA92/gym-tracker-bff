d8dd4e68b5adb53c1c819a3d41291577f2a2feb6
PROFILE\abarajas <abarajas@profile.es>
2020-01-17 13:17:32 +0100

    Initial commit

---
+++ src/main/java/com/gym/tracker/GymTackerBffApplication.java
@@ -0,0 +1,13 @@
+package com.gym.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymTackerBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymTackerBffApplication.class, args);
+	}
+
+}
