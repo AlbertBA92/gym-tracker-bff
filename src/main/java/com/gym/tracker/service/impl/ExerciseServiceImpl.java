27810aaca6463730182a1e2b957d6fb1946b707d
PROFILE\abarajas <abarajas@profile.es>
2020-01-27 13:38:20 +0100

    Removed sets when exercise is removed

--- src/main/java/com/gym/tracker/service/impl/ExerciseServiceImpl.java
+++ src/main/java/com/gym/tracker/service/impl/ExerciseServiceImpl.java
@@ -11,46 +11,60 @@ import org.springframework.stereotype.Component;
 import com.gym.tracker.dto.ExerciseDTO;
 import com.gym.tracker.exception.EntityNotFoundException;
 import com.gym.tracker.model.Exercise;
+import com.gym.tracker.model.Set;
 import com.gym.tracker.repository.ExerciseRepo;
+import com.gym.tracker.repository.SetRepo;
 import com.gym.tracker.service.IExerciseService;
 
 @Component
 public class ExerciseServiceImpl implements IExerciseService {
 
 	@Autowired
-	private ExerciseRepo repo;
+	private ExerciseRepo exerciseRepo;
+
+	@Autowired
+	private SetRepo setRepo;
 
 	@Autowired
 	private ModelMapper modelMapper;
 
 	@Override
 	public ExerciseDTO addExerciseEntry(Exercise exercise) {
-		return modelMapper.map(repo.save(exercise), ExerciseDTO.class);
+		return modelMapper.map(exerciseRepo.save(exercise), ExerciseDTO.class);
 	}
 
 	@Override
 	public List<ExerciseDTO> getExercises() {
-		return repo.findAll().stream().map(x -> modelMapper.map(x, ExerciseDTO.class)).collect(Collectors.toList());
+		return exerciseRepo.findAll().stream().map(x -> modelMapper.map(x, ExerciseDTO.class)).collect(Collectors.toList());
 	}
 
 	@Override
 	public ExerciseDTO updateExercise(Exercise exercise) {
-		return modelMapper.map(repo.save(exercise), ExerciseDTO.class);
+		return modelMapper.map(exerciseRepo.save(exercise), ExerciseDTO.class);
 	}
 
 	@Override
 	public void removeExercise(String id) {
-		repo.deleteById(id);
+		Optional<Exercise> findById = exerciseRepo.findById(id);
+		if (findById.isPresent()) {
+			Exercise exercise = findById.get();
+			for (Set set : exercise.getSets()) {
+				setRepo.delete(set);
+			}
+			exerciseRepo.deleteById(id);
+		} else {
+			throw new EntityNotFoundException(id);
+		}
 	}
 
 	@Override
 	public boolean isPresent(String id) {
-		return repo.findById(id).isPresent();
+		return exerciseRepo.findById(id).isPresent();
 	}
 
 	@Override
 	public ExerciseDTO getById(String id) {
-		Optional<Exercise> opcional = repo.findById(id);
+		Optional<Exercise> opcional = exerciseRepo.findById(id);
 		if (opcional.isPresent()) {
 			return modelMapper.map(opcional.get(), ExerciseDTO.class);
 		} else {
